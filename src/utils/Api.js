ca4fb6b8df1081bc7b3e414f353d7d2afcdf8767
Amashi996 <amasandunikab@gmail.com>
2023-04-09 21:53:01 +0530

    backend deployed

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from "../actions/types";
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/users",
+  baseURL: `https://procsupport-api.onrender.com/api/users`,
   headers: {
     "Content-Type": "application/json",
   },
