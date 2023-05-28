97617e51d6d8ff776f29813cf2bd074ddf706e6b
Amashi996 <amasandunikab@gmail.com>
2023-05-28 09:57:03 +0530

    PROC-104 fetch user list for admin

--- src/components/auth/SupplierLogin.js
+++ src/components/auth/SupplierLogin.js
@@ -1,77 +1,105 @@
 import React, { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { supplierLogin } from "../../actions/supplierAuth";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { userTypes } from "../../utils/constants";
 
-const SupplierLogin = ({ supplierLogin, isAuthenticated }) => {
-  const [formData, setFormData] = useState({
-    supplierUsername: "",
-    supplierUserPassword: "",
-  });
+const SupplierLogin = () => {
+  const [supplierUsername, setUsername] = useState("");
+  const [supplierUserPassword, setPassword] = useState("");
 
-  const { supplierUsername, supplierUserPassword } = formData;
+  const navigate = useNavigate();
 
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    supplierLogin(supplierUsername, supplierUserPassword);
+  const onChangeID = (e) => {
+    setUsername(e.target.value);
   };
 
-  if (isAuthenticated) {
-    return <Navigate to="/adminHomePage" />;
-  }
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      supplierUsername,
+      supplierUserPassword,
+    };
+
+    if (!user.supplierUsername && !user.supplierUserPassword) {
+      alert("Login Failed! Please enter Credentials");
+    } else if (!user.supplierUserPassword) {
+      alert("Login Failed! Please enter Password");
+    } else if (!user.supplierUsername) {
+      alert("Login Failed! Please enter Username");
+    } else {
+      console.log(user);
+      try {
+        await axios
+          .post(`https://procsupport-api.onrender.com/api/suppliers/login`, {
+            supplierUsername,
+            supplierUserPassword,
+          })
+          .then((res) => {
+            if (res.status === 200) {
+              console.log(res.data);
+              //const userType = res.data.data.usertype;
+              localStorage.setItem("token", res.data.data.token);
+
+              navigate("/");
+            } else {
+              alert("Login Failed! Please try again!");
+            }
+          });
+      } catch (e) {
+        if (e.response?.status === 401) {
+          alert("Login Failed! Please enter the correct password");
+        } else if (e.response?.status === 404) {
+          alert("Login Failed! Please enter a correct username");
+        } else {
+          alert("Login Failed! Please try again!");
+        }
+      }
+    }
+  };
 
   return (
-    <section className="container">
-      <h1 className="large text-primary">Sign In</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Sign Into Supplier Account
-      </p>
-      <form className="form" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control form-control-sm"
-            placeholder="Enter supplier username"
-            name="supplierUsername"
-            value={supplierUsername}
-            onChange={onChange}
-            style={{ width: "680px" }}
-          />
+    <div className="slogin">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Supplier Sign in</h1>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Enter username"
+                  name="supplierUsername"
+                  value={supplierUsername}
+                  onChange={onChangeID}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  name="supplierUserPassword"
+                  value={supplierUserPassword}
+                  onChange={onChangePassword}
+                />
+              </div>
+              <input
+                type="submit"
+                className="btn btn-success btn-block mt-4"
+                value={"Login"}
+              />
+            </form>
+          </div>
         </div>
-        <div className="form-group">
-          <input
-            type="password"
-            className="form-control form-control-sm"
-            placeholder="Enter supplier password"
-            name="supplierUserPassword"
-            value={supplierUserPassword}
-            onChange={onChange}
-            style={{ width: "680px" }}
-            minLength="3"
-          />
-        </div>
-        <input
-          type="submit"
-          className="btn btn-success btn-block mt-4"
-          style={{ width: "680px" }}
-          value="Login"
-        />
-      </form>
-    </section>
+      </div>
+    </div>
   );
 };
 
-SupplierLogin.propTypes = {
-  supplierLogin: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool,
-};
-
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthenticated,
-});
-
-export default connect(mapStateToProps, { supplierLogin })(SupplierLogin);
+export default SupplierLogin;
